8917b845d32c231fdc558eebf2c136dbc6267079
OGTrizzy <ogtrizzy420@gmail.com>
2024-02-05 14:42:49 +0100

    hmm

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-fetch('https://docs.noroff.dev/ecom-endpoints/square-eyes')
+fetch('https://api.noroff.dev/api/v1/square-eyes')
       .then(response => response.json())
       .then(json => console.log(json))
-
-      function sumNumbers(numbers) {
-        return numbers.reduce((total, number) => total + number, 0);
-      }
-      
-  
-      console.log(sumNumbers([1, 2, 3, 4])); 
